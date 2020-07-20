6456ee3f449b384a08d8896109327ad6175e95ec
PeerLess <poi7521@sina.cn>
2020-07-20 21:13:35 +0800

    1.2

---
+++ new.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+	> File Name: new.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 一  7/20 21:13:14 2020
+ ************************************************************************/
+
+#define inf 0x3f3f3f3f
+#include<bits/stdc++.h>
+using namespace std;
+#define SIS std::ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+typedef long long ll;
+
+
+
+int main(int argc, char *argv[]) {
+    SIS;
+
+
+
+adf
+    return 0;
+}
